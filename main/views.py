af45d4711834dbb6ffe7989d335c72d819748862
Wang Yingfei <clause@sjtu.edu.cn>
2014-07-06 23:40:58 +0800

    fix bugs
    	1. choice saved to database always true
    	2. multiple choices
    	3. bet topics already due

--- main/views.py
+++ main/views.py
@@ -62,17 +62,21 @@ def index(request):
 		title = request.GET['title']
 		topics = topics.filter(title__icontains=title)
 	topics = topics[:6]
-	return render(request, 'index.html', {'topics': topics, 'form': form},)
+	return render(request, 'index.html', {'topics': topics, 'form': form})
 
 def choose(request):
 	if request.method == 'POST':
 		if request.user.is_authenticated():
-			user_id = request.user.id
-			topic_id = request.POST['id']
-			choice = bool(request.POST['choice'])
+			user_ID = request.user.id
+			topic_ID = request.POST['id']
+			if Player_Topic.objects.filter(user_id = user_ID).exists():
+				return HttpResponse('You have already bet this topic')
+			if Topic.objects.get(topic_id = topic_ID).due < datetime.datetime.now():
+				return HttpResponse('This topic is already due')
+			choice = bool(int(request.POST['choice']))
 			new = Player_Topic()
-			new.user_id = user_id
-			new.topic_id = topic_id
+			new.user_id = user_ID
+			new.topic_id = topic_ID
 			new.choice = choice
 			new.save()
 			return HttpResponseRedirect('/index/')
